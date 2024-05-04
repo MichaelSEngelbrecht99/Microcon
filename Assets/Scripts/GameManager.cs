5bb575e4b423d851bf7f987dd618986f4df9f79d
Michael Engelbrecht <michaelsengelbrecht@gmail.com>
2024-05-04 17:22:37 +0200

    Animations, Game Mode Setup, & More
    - Added animation events
    - Imported LeanTween for animations
    - Added Platform, SaveManager, & ScoreManager scripts
    - Updated setup sequence to include saving

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,50 +1,70 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    #region Game Configuration Config/UI/Effects
     [Header("Game Configuration")]
     public GameModes SelectedGameMode;
     public GameDifficulties SelectedDifficulty;
     public Color[] AvailableColors;
-    [Space(24)]
+    [Space(14)]
 
     [Header("Card Prefab & Transform")]
     [SerializeField] private GameObject _cardPrefab;
+    [SerializeField] private float _cellPadding;
     private Dictionary<string, Card> _spawnedCards;
-    [Space(24)]
+    [Space(14)]
     [Header("Card Prefab & Transform")]
-    [SerializeField] private Grid _cardGrid;
-    [Space(24)]
+    [SerializeField] private GridLayoutGroup _cardGrid;
+
+    [Header("LeanTween Effects & Events")]
+    public UnityEvent CardSpawnIn;
+    public UnityEvent CardSpawnOut;
+    [SerializeField] private LeanTweenEffect PointerEnterEffect;
+    [SerializeField] private LeanTweenEffect PointerExitEffect;
+    [SerializeField] private LeanTweenEffect ClickEffect;
+    [SerializeField] private LeanTweenEffect SpawnInEffect;
+    #endregion
+    #region Debugging Items & Variables
+    [Space(14)]
     [Header("Debugging List")]
+    [SerializeField] private int _totalCards;
+    [SerializeField] private int _rows;
+    [SerializeField] private int _columns;
+    [SerializeField] private float _cellSizeRatio = 0.3f; // Adjust this value as needed
+
+    [SerializeField] private List<Card> _Cards;
+    public List<Card> Cards => _Cards;
     [SerializeField] private List<Card> _selectedCards;
     public List<Card> SelectedCards => _selectedCards;
     [SerializeField] private List<Card> _matchedCards;
     public List<Card> MatchedCards => _matchedCards;
-
-    private int _totalCards;
-    private int _rows;
-    private int _columns;
-
+    #endregion
+    #region Game Enums
     public enum GameModes
     {
-        Normal,
-        Endless,
-        TimeBased,
-        MineSweeper
+        None = 0,
+        Normal = 1,
+        Endless = 2,
+        TimeBased = 3,
+        MineSweeper = 4,
     }
 
     public enum GameDifficulties
     {
-        Easy,
-        Normal,
-        Medium,
-        Hard,
-        Extreme
+        None = 0,
+        Easy = 1,
+        Normal = 2,
+        Medium = 3,
+        Hard = 4,
+        Extreme = 5,
     }
 
     [Serializable]
@@ -58,40 +78,89 @@ public class GameManager : MonoBehaviour
     public struct Card
     {
         public string Id;
+        public Vector2 GridPosition;
         public bool IsMine;
         public bool Matched;
+        public Image CardImage;
+        public RectTransform CardRectTransform;
         public EventTrigger EventTrigger;
-        public GameObject GameObject;
+        public GameObject CardObject;
     }
 
-    public void Initialize()
+    [Serializable]
+    public struct LeanTweenEffect
     {
-        SetupGrid(SelectedGameMode, SelectedDifficulty);
+        public LeanTweenType SelectedEffect;
+        public float Delay;
+        public Vector3 SetScale;
+        public AudioClip EffectClip;
+    }
+    #endregion
+    void Start()
+    {
+        StartCoroutine(Initialize());
     }
 
-    public void SetupGrid(GameModes gameMode, GameDifficulties gameDifficulty)
+    #region Initialization
+    public IEnumerator Initialize()
+    {
+        yield return SetupGrid(SelectedGameMode, SelectedDifficulty);
+        AdjustGridLayoutGroup();
+        StartCoroutine(StartGame());
+    }
+
+    private IEnumerator SetupGrid(GameModes gameMode, GameDifficulties gameDifficulty, List<Card>? cards = null)
     {
         GridSize gridSize = GetGridSize(gameDifficulty);
         _rows = gridSize.Rows;
         _columns = gridSize.Columns;
 
+        int allowedColors = (_rows * _columns) / 2;
+
         _totalCards = _rows * _columns;
+
+        // Clear previous cards if any
+        foreach (Transform child in _cardGrid.transform)
+        {
+            Destroy(child.gameObject);
+        }
 
         // Spawn cards
         _spawnedCards = new Dictionary<string, Card>();
-        for (int i = 0; i < _totalCards; i++)
+        int cardIndex = 0;
+        for (int row = 0; row < _rows; row++)
         {
-            GameObject cardGO = Instantiate(_cardPrefab, _cardGrid.transform);
-            Card card = new Card
+            for (int col = 0; col < _columns; col++)
             {
-                Id = i.ToString(),
-                IsMine = false,
-                Matched = false,
-                GameObject = cardGO,
-                EventTrigger = cardGO.GetComponent<EventTrigger>()
-            };
-            _spawnedCards.Add(card.Id, card);
-            AddEventTriggers(card);
+                yield return new WaitForSeconds(0.03f);
+                GameObject cardGameObject = Instantiate(_cardPrefab, _cardGrid.transform);
+                RectTransform cardRectTransform = cardGameObject.GetComponent<RectTransform>();
+                cardRectTransform.localScale = Vector3.zero; // Set local scale to zero
+                Card card = new Card
+                {
+                    Id = cardIndex.ToString(),
+                    IsMine = false,
+                    Matched = false,
+                    CardObject = cardGameObject,
+                    CardImage = cardGameObject.transform.GetChild(0).GetChild(0).GetComponent<Image>(),
+                    CardRectTransform = cardRectTransform,
+                    EventTrigger = cardGameObject.GetComponent<EventTrigger>(),
+                    GridPosition = new Vector2(row, col)
+                };
+                _spawnedCards.Add(card.Id, card);
+                AddEventTriggers(card);
+
+                cardIndex++;
+            }
+        }
+
+        // If cards are provided for loading, update the grid accordingly
+        if (cards != null && cards.Count == _totalCards)
+        {
+            for (int i = 0; i < _totalCards; i++)
+            {
+                _spawnedCards[i.ToString()] = cards[i];
+            }
         }
 
         // Setup specific game mode
@@ -114,6 +183,37 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    private IEnumerator StartGame()
+    {
+        yield return new WaitForSeconds(SpawnInEffect.Delay);
+
+        foreach (Card card in _spawnedCards.Values)
+        {
+            card.CardRectTransform.localScale = Vector3.one;
+            LeanTween.scale(card.CardObject, SpawnInEffect.SetScale, SpawnInEffect.Delay).setEase(SpawnInEffect.SelectedEffect);
+        }
+    }
+
+    private void AdjustGridLayoutGroup()
+    {
+        // Calculate cell size based on grid dimensions
+        float cellSizeX = (_cardGrid.GetComponent<RectTransform>().rect.width / _columns) + _cellPadding;
+        float cellSizeY = (_cardGrid.GetComponent<RectTransform>().rect.height / _rows) + _cellPadding;
+        Debug.Log(cellSizeX);
+        Debug.Log(cellSizeY);
+        // Use the smaller dimension as the cell size
+        float cellSize = Mathf.Min(cellSizeX, cellSizeY);
+        Debug.Log(cellSize);
+        // Ensure cell size is valid
+        if (cellSize <= 0f)
+        {
+            Debug.LogWarning("Invalid cell size. Adjusting to default value.");
+            cellSize = 100f; // Default cell size value
+        }
+
+        _cardGrid.constraintCount = _columns;
+        _cardGrid.cellSize = new Vector2(cellSize, cellSize);
+    }
     private GridSize GetGridSize(GameDifficulties difficulty)
     {
         switch (difficulty)
@@ -132,7 +232,8 @@ public class GameManager : MonoBehaviour
                 return new GridSize { Rows = 2, Columns = 2 };
         }
     }
-
+    #endregion
+    #region Triggers
     private void AddEventTriggers(Card card)
     {
         EventTrigger trigger = card.EventTrigger;
@@ -159,19 +260,18 @@ public class GameManager : MonoBehaviour
         pointerClick.callback.AddListener((eventData) => { OnPointerClick(card); });
         trigger.triggers.Add(pointerClick);
     }
-
     private void OnPointerEnter(Card card)
     {
         // Implement pointer enter logic
-    }
 
+    }
     private void OnPointerExit(Card card)
     {
         // Implement pointer exit logic
     }
-
     private void OnPointerClick(Card card)
     {
         // Implement pointer click logic
     }
+    #endregion
 }
